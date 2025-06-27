0cee3d215242db2beeb1b332611371354369742f
Windyy-1007 <namphongtip02@gmail.com>
2025-06-27 09:48:24 +0700

    [DELETE] Deleted some unecessay files

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gunicorn -c gunicorn-cfg.py app_gpu:app
-
--- test.py
+++
@@ -1,4 +0,0 @@
-import torch
-ckpt = torch.load('./checkpts/hifigan.pt', map_location='cpu')
-print(type(ckpt))
-print(ckpt.keys() if isinstance(ckpt, dict) else "Not a dict")
